11eb9ec31045a66e26d3315b37e1206b1f5283e7
singh, Ashwini <ashwini.singh@externals.hilti.com>
2023-09-07 12:55:07 +0530

    Initial commit

---
+++ src/sampleApp/HelloWorld.java
@@ -0,0 +1,34 @@
+package sampleApp;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class HelloWorld {
+
+    static WebDriver setChromeDriver(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\priya.sharma\\Downloads\\chromedriver.exe");
+        return new ChromeDriver();
+    }
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        WebDriver driver = setChromeDriver();
+        driver.get("https://htmlcolorcodes.com/");
+        driver.manage().window().maximize();
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile,new File("D:\\test.png"));
+        driver.switchTo().newWindow(WindowType.TAB); //open a new tab
+//        driver.switchTo().newWindow(WindowType.WINDOW); //open new window
+//        ((JavascriptExecutor) driver).executeScript("window.open()");
+        Thread.sleep(5000);
+        driver.quit();
+
+    }
+}
---
+++ src/sampleApp/framesPractice.java
@@ -0,0 +1,49 @@
+package sampleApp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import sampleApp.HelloWorld;
+
+import java.util.concurrent.TimeUnit;
+
+public class framesPractice {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = HelloWorld.setChromeDriver();
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        driver.get("https://www.hyrtutorials.com/p/frames-practice.html");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+
+//        driver.switchTo().frame(driver.findElement(By.id("frm1")));
+//        Select sc = new Select(driver.findElement(By.id("selectnav2")));
+//        sc.selectByVisibleText("Home");
+//        Thread.sleep(2000);
+//        driver.switchTo().defaultContent();
+//        WebElement name = driver.findElement(By.id("name"));
+//        name.sendKeys("Hello world");
+//        Thread.sleep(2000);
+//        int size = driver.findElements(By.tagName("iframe")).size();
+//        System.out.println("Size:-"+size);
+//        js.executeScript("window.scrollBy(0,2050)","");
+//        js.executeScript("arguments[0].scrollIntoView(true);",example);
+
+//--------------------nested frames--------------------------------------
+        driver.switchTo().frame(driver.findElement(By.id("frm3")));
+        driver.switchTo().frame(driver.findElement(By.id("frm2")));
+        WebElement element = driver.findElement(By.name("fName"));
+        element.sendKeys("firstname");
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.switchTo().parentFrame();
+        WebElement element1 = driver.findElement(By.id("name"));
+        element1.sendKeys("helloWorld");
+        Thread.sleep(2000);
+        driver.switchTo().defaultContent();
+        WebElement element2 = driver.findElement(By.id("name"));
+        element2.sendKeys("helloWorld");
+
+    }
+}
